66172e15446cb9a01877e2eb181800b055ce0e9b
Dipikagithub123 <155236376+Dipikagithub123@users.noreply.github.com>
2024-06-28 22:43:57 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ExampleContract {
+contract mySmartContract {
     uint public balance;
 
     // Function to deposit Ether into the contract
